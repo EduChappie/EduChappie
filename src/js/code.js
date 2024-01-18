5a871830d113d514eaf5c3805b20699438d097a9
EduChappie <chappiefevplus@gmail.com>
2024-01-18 12:52:17 -0300

    main part 1

---
+++ src/js/code.js
@@ -0,0 +1,16 @@
+// Código de link das midias sociais
+function socialMedia(n) {
+    switch (n) {
+        case 1:
+            window.open("https://github.com/EduChappie");
+            break;
+        case 3:
+            window.open("https://www.instagram.com/edu.f21/")
+            break;
+    }
+}
+
+//opção de rolagem
+function soScroll(num) {
+    window.scrollTo(0, num);
+}
